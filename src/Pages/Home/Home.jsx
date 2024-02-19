08aa2bb462a2c266a8394ce634fe615bba287e09
Alfaz Hossain <alfaz.cse@gmail.com>
2024-02-19 17:43:22 +0600

    basic setup

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-5xl">Hello</h2>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/routers/Router.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
+
+
+const router = createBrowserRouter([
+  {
+   path: "/",
+   element: <MainLayout></MainLayout>,
+   errorElement: <ErrorPage></ErrorPage>,
+   children:[
+    {
+      path:"/",
+      element: <Home></Home>
+    },
+    {
+      path:"/login",
+      element: <Login></Login>
+    },
+    {
+      path:"/signUp",
+      element: <SignUp></SignUp>
+    },
+   ]
+  },
+  ]);
+
+export default router;
